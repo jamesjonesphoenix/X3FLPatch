d973db99af5c7fab81eb5e3b4c6d7b3c1edf86c1
Cycrowuk <166267499+Cycrowuk@users.noreply.github.com>
2025-05-17 19:51:03 +0100

    Added Build scripts

---
+++ BUILD/build.bat
@@ -0,0 +1,15 @@
+@Echo off
+catpck -a ../types/*.txt ../06.cat::addon2/types/
+catpck -a ../maps/*.xml ../06.cat::addon2/maps/
+catpck -a ../cutscenes/*.xml ../06.cat::addon2/cutscenes/
+catpck -a ../cutscenes/*.xsd ../06.cat::addon2/cutscenes/
+catpck -a ../director/images/* ../06.cat::addon2/director/images/
+catpck -a ../director/*.xml ../06.cat::addon2/director/
+catpck -a ../director/*.xsd ../06.cat::addon2/director/
+catpck -a ../L/x3galedit.obj ../06.cat::addon2/L/
+catpck -a ../L/x3intro.obj ../06.cat::addon2/L/
+catpck -a ../L/x3story.obj ../06.cat::addon2/L/
+catpck -a ../t/0001* ../06.cat::addon2/t/
+catpck -a ../t/0002* ../06.cat::addon2/t/
+catpck -a ../t/9998* ../06.cat::addon2/t/
+spktool -createscript patch.sps
